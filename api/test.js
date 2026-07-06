54854ce291368627b80ef90f470ca9e728180a17
daudonhere <hellodadedaud@gmail.com>
2026-07-06 13:54:16 +0700

    chore: clean up debug test files

--- api/test.js
+++
@@ -1,5 +0,0 @@
-export default function handler() {
-  return new Response(JSON.stringify({ js: true }), {
-    headers: { 'Content-Type': 'application/json' },
-  })
-}
--- api/test.mjs
+++
@@ -1,5 +0,0 @@
-export default function handler() {
-  return new Response(JSON.stringify({ mjs: true }), {
-    headers: { 'Content-Type': 'application/json' },
-  })
-}
